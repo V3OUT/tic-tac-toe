8df8d8a38891ad1877d71760637e768d7ac926b6
“[yatharth <[yatharthvaish@gmail.com]>
2024-06-24 16:45:39 +0530

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TictactoeComponent } from './tictactoe/tictactoe.component';
@@ -12,6 +12,7 @@ import { TictactoeComponent } from './tictactoe/tictactoe.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [
--- src/app/tictactoe/tictactoe.component.ts
+++ src/app/tictactoe/tictactoe.component.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
 })
 export class TictactoeComponent 
 {
+  playerA:player = new player('Player A', 'x', false, false);
+  playerB:player = new player('Player B', 'o', false, false);
+
+  updateplayername(playerlabel:string)
+  {
+    if (playerlabel=="A")
+      {
+        this.playerA.isEditing = true;
+        this.playerB.isEditing = false;
+      }
+    if (playerlabel=="B")
+      {
+        this.playerA.isEditing = false;
+        this.playerB.isEditing = true;
+      }
+  }
+}
+
+class player
+{
+  name: string;
+  symbol: string; //x or o
+  isturn: boolean;
+  isEditing:boolean;
+  constructor(name: string, symbol: string,isturn: boolean,isEditing: boolean)
+  {
+    this.name = name;
+    this.symbol = symbol;
+    this.isturn = isturn;
+    this.isEditing = isEditing;
+  }
+
   
+
 }
